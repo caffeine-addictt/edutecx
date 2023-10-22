b51a0fee37b962e2e42d6314815994689657b9d0
Alex <taboez4life@gmail.com>
2023-10-22 16:32:22 +0800

    + testing exponential backoff algorithm

---
+++ test/test_utils.py
@@ -0,0 +1,28 @@
+def test_exponentialBackoff_success():
+  import src.util.utils as s
+
+  def check(x):
+    return x == 1
+
+  @s.exponential_backoff(retries = 1, validate = check)
+  def func():
+    return 1
+
+  assert func() == 1
+
+
+def test_exponentialBackoff_failure():
+  import src.util.utils as s
+
+  def check(x):
+    return x == 1
+
+  @s.exponential_backoff(retries = 1, validate = check)
+  def func():
+    return 2
+  
+  try:
+    func()
+    assert 1 == 0, 'Function did not raise an ExponentialBackoffError'
+  except s.ExponentialBackoffError:
+    assert 1 == 1
