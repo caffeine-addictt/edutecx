cec0868c8715f71b896175e13dbed7514ec76d3e
Alex <taboez4life@gmail.com>
2023-10-29 01:04:11 +0800

    + Update imports for new db models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -4,3 +4,8 @@ Load database modules
 
 from .user import UserModel
 from .token import TokenModel
+from .classroom import ClassroomModel
+from .comment import CommentModel
+from .submission import SubmissionModel
+from .assignment import AssignmentModel
+from .document import DocumentModel
