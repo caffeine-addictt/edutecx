f087fc12074705fdda594ff56d81a39e1ecd9d93
Alex <taboez4life@gmail.com>
2023-10-29 01:03:37 +0800

    + Testing DB cleanup + Testing DB for classroomModel

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,13 @@
 import pytest
 from flask import Flask
 
-
 # testing app startup
 @pytest.fixture()
 def app():
   from src import init_app
-  yield init_app(testing = True)
+  return init_app(testing = True)
 
 @pytest.fixture()
 def client(app: Flask):
-  app.testing = True
-  yield app.test_client()
+  with app.test_client() as client:
+    yield client
--- test/test_database.py
+++ test/test_database.py
@@ -3,7 +3,35 @@ Testing for database models
 """
 
 from flask import Flask
-from flask.testing import FlaskClient
+from flask_sqlalchemy import SQLAlchemy
+
+from typing import Callable, Any
+
+def withCleanup(app: Flask, db: SQLAlchemy, cleanup: None | Callable[[], None] = None) -> Callable[[Callable[..., Any]], Any]:
+  """
+  Wrapping decorator for testing database
+
+  handles wrapping with app and db cleanup
+  """
+  def decorator(func: Callable[..., Any]) -> Callable[..., None]:
+    def wrapper(*args: Any, **kwargs: Any) -> None:
+      with app.app_context():
+        try:
+          func(*args, **kwargs)
+        except Exception as e:
+          db.session.reset()
+          db.session.rollback()
+          if cleanup: cleanup(); db.session.commit()
+          raise e
+        finally:
+          db.session.reset()
+          db.session.rollback()
+          if cleanup: cleanup(); db.session.commit()
+    return wrapper
+  return decorator
+
+
+
 
 def test_userModel(app: Flask):
   """
@@ -12,7 +40,8 @@ def test_userModel(app: Flask):
   from src import db
   from src.database import UserModel
 
-  with app.app_context():
+  @withCleanup(app, db)
+  def run():
     userNew = UserModel(
       email = 'user_test_user@example.com',
       username = 'user_test_user',
@@ -24,10 +53,10 @@ def test_userModel(app: Flask):
     foundUser: UserModel | None = UserModel.query.filter(UserModel.username == 'user_test_user').first()
     isSame: bool = (foundUser is not None) and (foundUser.id == userNew.id)
 
-    UserModel.query.filter(UserModel.id == userNew.id).delete()
     assert isSame, f'Found User is {foundUser}'
 
-  
+  run()
+
 
 def test_token_User_Relationship(app: Flask):
   """
@@ -36,7 +65,8 @@ def test_token_User_Relationship(app: Flask):
   from src import db
   from src.database import UserModel, TokenModel
 
-  with app.app_context():
+  @withCleanup(app, db)
+  def run():
     userNew = UserModel(
       email = 'token_test_user@example.com',
       username = 'token_test_user',
@@ -44,14 +74,12 @@ def test_token_User_Relationship(app: Flask):
       privilege = 'User'
     )
     db.session.add(userNew)
-    db.session.commit()
 
     tokenNew = TokenModel(
       user = userNew,
       token_type = 'Verification'
     )
     db.session.add(tokenNew)
-    db.session.commit()
 
 
     searchedUser: UserModel | None = UserModel.query.filter(UserModel.username == 'token_test_user').first()
@@ -73,16 +101,75 @@ def test_token_User_Relationship(app: Flask):
     else:
       log.append('User was not mapped to tokenModel')
 
-
-    # Cleanup
-    TokenModel.query.filter(TokenModel.id == tokenNew.id).delete()
-    UserModel.query.filter(UserModel.id == userNew.id).delete()
-    db.session.commit()
-
     # Assertion
     assert len(log) == 0, '\n'.join(log)
 
+  run()
 
 
+def test_classroom_user_relationship(app: Flask):
+  """
+  Testing for User-Classroom DB Schema relationships & Classroom model writing to database
+  """
+  from src import db
+  from src.database import UserModel, ClassroomModel
 
+  def clean():
+    UserModel.query.filter(UserModel.username == 'classroom_test_user').delete()
+    UserModel.query.filter(UserModel.username == 'classroom_test2_user').delete()
+    UserModel.query.filter(UserModel.username == 'classroom_test3_user').delete()
+    ClassroomModel.query.filter(ClassroomModel.title == 'testing').delete()
+
+  @withCleanup(app, db, clean)
+  def run():
+    userOne = UserModel(
+      email = 'classroom_test_user@example.com',
+      username = 'classroom_test_user',
+      password = '<PASSWORD>',
+      privilege = 'User'
+    )
+    db.session.add(userOne)
+    db.session.commit()
+
+    userTwo = UserModel(
+      email = 'classroom_test_user2@example.com',
+      username = 'classroom_test2_user',
+      password = '<PASSWORD>',
+      privilege = 'User'
+    )
+    db.session.add(userTwo)
+    db.session.commit()
+
+    userThree = UserModel(
+      email = 'classroom_test_user3@example.com',
+      username = 'classroom_test3_user',
+      password = '<PASSWORD>',
+      privilege = 'User'
+    )
+    db.session.add(userThree)
+    db.session.commit()
+
+    classNew = ClassroomModel(
+      owner = userOne,
+      title = 'testing',
+      description = 'desc'
+    )
+    db.session.add(classNew)
+    db.session.commit()
+
+    classNew.add_educators(userTwo)
+    classNew.add_students(userThree)
+    db.session.commit()
+
+
+    # Check
+    foundClassroom: ClassroomModel | None = ClassroomModel.query.filter(ClassroomModel.id == classNew.id).first()
+
+    assert foundClassroom and foundClassroom.students
+    assert foundClassroom and len(foundClassroom.students) == 1
+
+    assert foundClassroom and foundClassroom.educators
+    assert foundClassroom and len(foundClassroom.educators) == 1
+
+  run()
 
