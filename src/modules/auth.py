8d959f7cc5cf21b52261594cbad0c173d36941a2
Alex <taboez4life@gmail.com>
2023-10-29 14:31:16 +0800

    + login form logic

--- src/modules/auth.py
+++ src/modules/auth.py
@@ -2,9 +2,12 @@
 Managing user login/logout
 """
 
-from .. import loginManager
-from ..utils.http import HTTPStatusCode
-from ..database import UserModel
+from src import loginManager
+from src.database import UserModel
+from src.utils.http import HTTPStatusCode
+from src.utils.forms import LoginForm, RegisterForm
+
+from typing import Optional
 
 from urllib import parse
 from custom_lib.flask_login import login_user, logout_user, login_required, current_user
@@ -24,7 +27,7 @@ def beforeRequest():
   g.user = current_user
 
 @loginManager.user_loader
-def loadUser(userID: str) -> UserModel | None:
+def loadUser(userID: str) -> Optional['UserModel']:
   # Placed in try-except block to prevent edge case of invalid formatting of userID
   try: return UserModel.query.get(userID)
   except Exception as e:
@@ -37,19 +40,26 @@ def loadUser(userID: str) -> UserModel | None:
 @app.route('/login', methods = ['Get', 'POST'])
 def login():
   # Auto redirect to callbackURI
-  if g.user.is_authenticated():
+  if g.user.is_authenticated:
     callbackURI: str = parse.quote(request.args.get('callbackURI', '/'))
     return redirect(callbackURI, code = HTTPStatusCode.PERMANENT_REDIRECT)
   
-  # TODO: Form Logic
-  form: None
+  form = LoginForm(request.form)
 
-  return render_template('(auth)/login/index.html')
+  if form.validate_on_submit():
+    user: Optional['UserModel'] = UserModel.query.filter_by(email = form.email.data).first()
+
+    if user is not None and user.verify_password(form.password.data):
+      login_user(user)
+      callbackURI: str = parse.quote(request.args.get('callbackURI', '/'))
+      return redirect(callbackURI, code = HTTPStatusCode.PERMANENT_REDIRECT)
+
+  return render_template('(auth)/login/index.html', form = form)
 
 
 @app.route('/logout', methods = ['GET', 'POST'])
 def logout():
-  if g.user.is_authenticated():
+  if g.user.is_authenticated:
     logout_user()
     return redirect('/', code = HTTPStatusCode.PERMANENT_REDIRECT)
   
