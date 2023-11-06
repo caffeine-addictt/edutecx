8f9d4e25214ff3e5851108716ccdfaa4db9c8dc3
I-like-coffee <taboez4life@gmail.com>
2023-11-07 01:00:44 +0800

    + Access token expirey to 1h

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ class ConfigBase:
   # Docs https://flask-jwt-extended.readthedocs.io/en/3.0.0_release/options/
   JWT_SECRET_KEY: str = 'jwt-secret'
   JWT_TOKEN_LOCATION: list[str] = ['headers', 'cookies']
-  JWT_ACCESS_TOKEN_EXPIRES: int = 1 * 60 # 1h in seconds
+  JWT_ACCESS_TOKEN_EXPIRES: int = 60 * 60 # 1h in seconds
   JWT_REFRESH_TOKEN_EXPIRES: int = 30 * 24 * 60 * 60 #30 days in seconds
   PROPAGATE_EXCEPTIONS: bool = True
 
