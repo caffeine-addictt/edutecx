ae58e3626e7081e0f5dcabdfbc2da67f6cc5bd6e
Alex <taboez4life@gmail.com>
2023-11-07 20:25:37 +0800

    + add auto refresh access token

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -4,13 +4,13 @@ Managing user login/logout
 ! Note to self: redirects from POST -> GET needs code 302/303 to change request body as 307/308 preserves the original body
 """
 
-from src import jwt
+from src import db, jwt
 from src.database import UserModel, JWTBlocklistModel
 
 from src.utils.ext import utc_time
 from src.utils.http import HTTPStatusCode, APIResponse
 from src.utils.forms import LoginForm, LogoutForm, RegisterForm
-from src.service.auth_provider import optional_jwt, require_login
+from src.service.auth_provider import optional_jwt
 
 import requests
 from urllib import parse
@@ -23,13 +23,16 @@ from flask import (
   current_app as app,
 )
 from flask_jwt_extended import (
-  get_jwt,
+  decode_token,
   jwt_required,
   get_jwt_identity,
+
   unset_jwt_cookies,
+  unset_access_cookies,
+  unset_refresh_cookies,
+
   set_access_cookies,
   set_refresh_cookies,
-  create_access_token,
 )
 
 
@@ -45,21 +48,48 @@ def user_lookup_loader(jwtHeader, jwtPayload):
 
 @app.after_request
 def refresh_token(response):
+  if request.method != 'GET' or request.path.startswith('/api/') or request.path.startswith('/static'):
+    return response
+  
   try:
-    exp = get_jwt()['exp']
-    if utc_time.skip('30minutes').timestamp() > exp:
-      access_token = create_access_token(identity = get_jwt_identity())
-      set_access_cookies(response, access_token)
+    access_token = request.cookies.get('access_token_cookie')
+    refresh_token = request.cookies.get('refresh_token_cookie')
+
+    if not access_token or not refresh_token:
+      raise Exception('Not logged in')
+    
+    access_decoded = decode_token(access_token, allow_expired = True)
+    refresh_decoded = decode_token(refresh_token)
+
+    if refresh_decoded['exp'] <= utc_time.get().timestamp():
+      raise Exception('Refresh token expired')
+
+    if utc_time.skip('30min').timestamp() < access_decoded['exp']:
+      raise Exception('Access token not close to being expired')
+    
+    # Try to refresh access token
+    refresh_response = requests.post(
+      f'{request.url_root}api/v1/refresh',
+      headers = {'Authorization': f'Bearer {refresh_token}'}
+    )
+    data: APIResponse = refresh_response.json()
+
+    if data['status'] != HTTPStatusCode.OK:
+      raise Exception(f'{data["message"]}')
+    
+    set_access_cookies(response, str((data.get('data') or {}).get('access_token')))
+    app.logger.info(f'Successfully Auto-Refreshed expiring access token')
 
     return response
-  except Exception:
+  except Exception as e:
+    app.logger.info(f'Failed to auto-refresh expiring access token: {e}')
     return response
 
 
 # Error handling
 @jwt.unauthorized_loader
 def unauthorized_loader(msg: str):
-  if request.method == 'POST':
+  if request.path.startswith('/api/'):
     return {
       'message': msg,
       'status': HTTPStatusCode.UNAUTHORIZED
@@ -67,12 +97,12 @@ def unauthorized_loader(msg: str):
   else:
     return redirect(
       f'/login?callbackURI={parse.quote(request.path)}',
-      code = HTTPStatusCode.TEMPORARY_REDIRECT
-    ), HTTPStatusCode.TEMPORARY_REDIRECT
+      code = HTTPStatusCode.SEE_OTHER
+    ), HTTPStatusCode.SEE_OTHER
 
 @jwt.invalid_token_loader
 def invalid_token_loader(msg: str):
-  if request.method == 'POST':
+  if request.path.startswith('/api/'):
     return {
       'message': msg,
       'status': HTTPStatusCode.UNPROCESSABLE_ENTITY
@@ -82,22 +112,56 @@ def invalid_token_loader(msg: str):
       f'/login?callbackURI={parse.quote(request.path)}',
       code = HTTPStatusCode.SEE_OTHER
     ))
-    unset_jwt_cookies(response)
     return response, HTTPStatusCode.SEE_OTHER
 
 @jwt.expired_token_loader
 def expired_token_loader(jwtHeader, jwtPayload):
-  if request.method == 'POST':
+  if request.path.startswith('/api/'):
     return {
       'message': 'Token has expired',
       'status': HTTPStatusCode.UNAUTHORIZED
     }, HTTPStatusCode.UNAUTHORIZED
   else:
+    try:
+      # Refresh the token if refresh token is valid and they previously logged in with "remember me" checked
+
+      accesss_token = request.cookies.get('access_token_cookie')
+      refresh_token = request.cookies.get('refresh_token_cookie')
+
+      if not accesss_token or not refresh_token:
+        raise Exception('No cookies')
+      
+      decoded_refresh = decode_token(refresh_token)
+      if not decoded_refresh.get('remember_me'):
+        raise Exception('Did not log in with remember me checked!')
+      
+      response = requests.post(
+        f'{request.root_url}api/v1/refresh',
+        headers = {'Authorization': f'Bearer {refresh_token}'}
+      )
+      data: APIResponse = response.json()
+
+      if (data['status'] == HTTPStatusCode.OK) and not (data.get('data') or {}).get('access_token'):
+        raise Exception('Unable to refresh')
+      
+      successfulRefresh = make_response(
+        redirect(request.path, code = HTTPStatusCode.FOUND),
+        HTTPStatusCode.FOUND
+      )
+
+      app.logger.error('Auto-Refreshed expired access token with refresh token')
+      set_access_cookies(successfulRefresh, str((data.get('data') or {}).get('access_token')))
+      return successfulRefresh, HTTPStatusCode.FOUND
+
+    except Exception as e:
+      app.logger.error(f'Failed to Auto-Refresh expired access token with refresh token: {e}')
+    
     response = make_response(redirect(
       f'/login?callbackURI={parse.quote(request.path)}',
       code = HTTPStatusCode.SEE_OTHER
     ))
-    unset_jwt_cookies(response)
+    unset_refresh_cookies(response)
+    unset_access_cookies(response)
     return response, HTTPStatusCode.SEE_OTHER
   
 @jwt.token_in_blocklist_loader
@@ -127,11 +191,11 @@ def login():
       headers = {'Content-Type': 'application/json'},
       json = {
         'email': form.email.data,
-        'password': form.password.data
+        'password': form.password.data,
+        'remember_me': form.remember_me.data,
       }
     )
     body: APIResponse = response.json()
-    app.logger.error(body)
     
     if response.status_code != HTTPStatusCode.OK:
       flash(body.get('message'), 'danger')
@@ -156,24 +220,49 @@ def login():
 
 
 @app.route('/logout', methods = ['GET', 'POST'])
-@require_login
-def logout(user: UserModel):
-  form = LogoutForm(request.form)
-  if request.method == 'POST' and form.validate_on_submit():
-    # Add to token blocklist
-    jti = get_jwt()['jti']
-    newBlacklist = JWTBlocklistModel(jti)
-    newBlacklist.save()
+def logout():
+  successfulLogout = make_response(
+    redirect('/', code = HTTPStatusCode.SEE_OTHER),
+    HTTPStatusCode.SEE_OTHER
+  )
 
-    successfulLogout = make_response(
-      redirect('/', code = HTTPStatusCode.SEE_OTHER),
-      HTTPStatusCode.SEE_OTHER
-    )
-    unset_jwt_cookies(successfulLogout)
+  # Not logged in
+  if (
+    not request.cookies.get('access_token_cookie')
+    and not request.cookies.get('refresh_token_cookie')
+  ):
+    return successfulLogout, HTTPStatusCode.SEE_OTHER
+  
+  # Logged in
+  if request.method == 'POST':
+    try:
+      accesss_token = request.cookies.get('access_token_cookie')
+      refresh_token = request.cookies.get('refresh_token_cookie')
 
-    flash('Successfully logged out', 'info')
-    return successfulLogout
-  return render_template('(auth)/logout.html')
+      if not accesss_token or not refresh_token:
+        raise Exception('No cookies')
+      
+      decoded_access = decode_token(accesss_token)
+      decoded_refresh = decode_token(refresh_token)
+
+      if decoded_access.get('jti'): db.session.add(JWTBlocklistModel(str(decoded_access.get('jti')), 'access'))
+      if decoded_refresh.get('jti'): db.session.add(JWTBlocklistModel(str(decoded_refresh.get('jti')), 'refresh'))
+      db.session.commit()
+
+      flash('Successfully logged out', 'info')
+
+    except Exception as e:
+      app.logger.exception(f'Failed to logout: {e}')
+    
+    finally:
+      unset_refresh_cookies(successfulLogout)
+      unset_access_cookies(successfulLogout)
+      unset_jwt_cookies(successfulLogout)
+
+      app.logger.warning('Unset cookies on logout')
+      return successfulLogout, HTTPStatusCode.SEE_OTHER
+    
+  return render_template('(auth)/logout.html', form = LogoutForm())
 
 
 @app.route('/register', methods = ['GET', 'POST'])
@@ -182,7 +271,7 @@ def register():
   # Redirect to callbackURI or home if logged in
   if get_jwt_identity():
     callbackURI: str = parse.quote(request.args.get('callbackURI', '/home'))
-    return redirect(callbackURI, code = HTTPStatusCode.PERMANENT_REDIRECT), HTTPStatusCode.PERMANENT_REDIRECT
+    return redirect(callbackURI, code = HTTPStatusCode.SEE_OTHER), HTTPStatusCode.SEE_OTHER
   
   form = RegisterForm(request.form)
   if request.method == 'POST' and form.validate_on_submit():
@@ -203,7 +292,7 @@ def register():
       flash(body.get('message'), 'success')
 
       callbackURI: str = parse.quote(request.args.get('callbackURI', '/login'))
-      return redirect(callbackURI, code = HTTPStatusCode.PERMANENT_REDIRECT), HTTPStatusCode.PERMANENT_REDIRECT
+      return redirect(callbackURI, code = HTTPStatusCode.FOUND), HTTPStatusCode.FOUND
   elif request.method == 'POST':
     flash('Failed to create user account', 'danger')
 
