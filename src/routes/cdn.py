cc2fab1df170abf1397a538bd514b43b506e53ae
Alex <taboez4life@gmail.com>
2023-11-13 03:39:49 +0800

    + CDN endpoint handler

---
+++ src/routes/cdn.py
@@ -0,0 +1,42 @@
+"""
+Handles user uploaded content serving
+"""
+
+from src.database import UserModel
+
+from flask import (
+  send_from_directory,
+  current_app as app
+)
+from werkzeug.exceptions import Unauthorized
+
+from src.service import auth_provider, cdn_provider
+cdn_provider._dirCheck()
+
+# Serving
+@app.route('/public/images/<path:filename>')
+def uploaded_images(filename: str):
+  return send_from_directory(
+    cdn_provider.TextbookLocation,
+    filename,
+    as_attachment = True
+  )
+
+@app.route('/public/textbooks/<path:filename>')
+@auth_provider.require_login
+def uploaded_textbooks(user: UserModel, filename: str):
+  if user.privilege == 'Admin':
+    return send_from_directory(
+      cdn_provider.TextbookLocation,
+      filename,
+      as_attachment = True
+    )
+  
+  if any([ i.iuri.endswith(filename) for i in user.textbooks ]):
+    return send_from_directory(
+      cdn_provider.TextbookLocation,
+      filename,
+      as_attachment = True
+    )
+  
+  else: raise Unauthorized()
