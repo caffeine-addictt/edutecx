89472d9cb1ad98d7b6f9b88f210cb5bedd309669
Alex <taboez4life@gmail.com>
2023-11-13 03:45:51 +0800

    + uploading textbook w/ cdn

--- src/database/textbook.py
+++ src/database/textbook.py
@@ -3,10 +3,12 @@ Textbook Model
 """
 
 from src import db
+from src.utils.ext.threading import Thread
+from src.service.cdn_provider import uploadTextbook
 
 import uuid
 from datetime import datetime
-from typing import Optional, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING, Union, List
 from werkzeug.datastructures import FileStorage
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -14,15 +16,16 @@ from sqlalchemy import (
   Float,
   String,
   DateTime,
-  ForeignKey,
-  LargeBinary
+  ForeignKey
 )
 
 # Import TokenModel at runtime to prevent circular imports
 if TYPE_CHECKING:
   from .user import UserModel
+  from .image import ImageModel
 
 
+# TODO: Textbook PDF upload
 class TextbookModel(db.Model):
   """
   Textbook Model
@@ -35,12 +38,14 @@ class TextbookModel(db.Model):
   author_id: Mapped[str] = mapped_column(ForeignKey('user_table.id'), nullable = False)
 
   # Attributes
-  price    : Mapped[float] = mapped_column(Float, nullable = False, default = 0.0)
-  discount : Mapped[float] = mapped_column(Float, nullable = False, default = 0.0)
-  author   : Mapped['UserModel'] = relationship('UserModel', back_populates = 'owned_textbooks')
-  filename : Mapped[str] = mapped_column(String, nullable = False, unique = True)
-  data     : Mapped[LargeBinary] = mapped_column(LargeBinary, nullable = False)
-  cover_img: Mapped[LargeBinary] = mapped_column(LargeBinary, nullable = True)
+  price      : Mapped[float] = mapped_column(Float, nullable = False, default = 0.0)
+  discount   : Mapped[float] = mapped_column(Float, nullable = False, default = 0.0)
+  author     : Mapped['UserModel'] = relationship('UserModel', back_populates = 'owned_textbooks')
+
+  uri        : Mapped[str]                    = mapped_column(String, nullable = True)
+  iuri       : Mapped[str]                    = mapped_column(String, nullable = True)
+  status     : Mapped[str]                    = mapped_column(String, nullable = False, default = 'Uploading')
+  cover_image: Mapped[Optional['ImageModel']] = relationship('ImageModel', back_populates = 'textbook')
 
   # Logs
   created_at: Mapped[datetime] = mapped_column(DateTime, nullable = False, default = datetime.utcnow)
@@ -50,25 +55,48 @@ class TextbookModel(db.Model):
     self,
     author: 'UserModel',
     file: FileStorage,
-    cover_img: Optional[FileStorage],
     price: float = 0.0,
     discount: float = 0.0,
   ) -> None:
     assert isinstance(price, float)
     assert isinstance(discount, float) and (0 <= discount <= 1)
-    assert (not cover_img) or isinstance(cover_img, FileStorage)
 
     self.author_id = author.id
     self.price = price
     self.discount = discount
-
-    # ? Or might it be better to save file to local $path and save the abs path to it?
-    self.filename = self.id + (file.filename or '.pdf')[-4:]
-    self.data = file.read()
-
-    if cover_img is not None:
-      self.cover_img = cover_img.read()
+    self._upload_handler(file)
       
-  def __repr__(self):
+  def __repr__(self) -> str:
     """To be used with cache indexing"""
     return '%s(%s)' % (self.__class__.__name__, self.id)
+  
+
+  def _upload_handler(self, file: FileStorage) -> None:
+    """Threaded background upload process"""
+    def updateURI(filePath: str):
+      self.iuri = filePath
+      self.status = 'Uploaded'
+      self.save()
+
+    filename = f'{self.id}-{self.author_id or ""}'
+
+    uploadJob = Thread(uploadTextbook)
+    uploadJob.add_hook(updateURI)
+    uploadJob.start(file = file, filename = filename)
+  
+  
+  # DB
+  def save(self) -> None:
+    """Commits the model"""
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self, commit: bool = True) -> None:
+    # """Deletes the model and its references"""
+    # if self.cover_image: self.cover_image.delete(commit = False)
+
+    # db.session.delete(self)
+    # if commit: db.session.commit()
+    
+    # Deleting textbooks are not allowed
+    pass
