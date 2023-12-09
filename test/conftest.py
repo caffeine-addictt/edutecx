1fc87b01ba8c41e5fbdc26170013e8f55ce7c0f7
AlexNg <taboez4life@gmail.com>
2023-12-09 15:02:26 +0800

    + Fix pytest

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,14 @@
 import pytest
 from flask import Flask
 
+from src import init_app
+flaskApp = init_app(testing = True)
+
 # testing app startup
 @pytest.fixture()
 def app():
-  from src import init_app
-  return init_app(testing = True)
+  yield flaskApp
 
 @pytest.fixture()
 def client(app: Flask):
-  with app.test_client() as client:
-    yield client
+  return app.test_client()
