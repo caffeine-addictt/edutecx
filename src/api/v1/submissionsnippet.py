4e1f554b938e3f595d156c416c834e6d0edb5c1e
AndrewAce <andrewkangmingyi@gmail.com>
2023-12-23 14:05:00 +0800

    Closes #65

--- src/api/v1/submissionsnippet.py
+++ src/api/v1/submissionsnippet.py
@@ -7,6 +7,7 @@ from src.database import SubmissionSnippetModel, UserModel
 from src.service.auth_provider import require_login
 from src.utils.http import HTTPStatusCode
 from src.utils.api import (
+  SubmissionSnippetGetReply, SubmissionSnippetGetRequest, _SubmissionSnippetGetData,
   GenericReply
 )
 
@@ -28,4 +29,35 @@ auth_limit = limiter.shared_limit('100 per hour', scope = lambda _: request.host
 @auth_limit
 @require_login
 def submissionsnippet_get_api(user: UserModel):
-  ...
+  req = SubmissionSnippetGetRequest(request)
+
+  submissionSnippet = SubmissionSnippetModel.query.filter(SubmissionSnippetModel.id == req.submissionSnippet_id).first()
+  if not submissionSnippet or not isinstance(submissionSnippet, SubmissionSnippetModel):
+    return GenericReply(
+      message = 'Unable to locate submission snippet',
+      status = HTTPStatusCode.BAD_REQUEST
+    ).to_dict(), HTTPStatusCode.BAD_REQUEST
+  
+  if (user.privilege != 'Admin') and (
+    user.id not in [
+      submissionSnippet.submission.assignment.classroom.owner_id,
+      *submissionSnippet.submission.assignment.classroom.educator_ids.split('|'),
+      *submissionSnippet.submission.assignment.classroom.student_ids.split('|')
+  ]):
+    return GenericReply(
+      message = 'Unauthorized',
+      status = HTTPStatusCode.UNAUTHORIZED
+    ).to_dict(), HTTPStatusCode.UNAUTHORIZED
+  
+  return SubmissionSnippetGetReply(
+    message = 'Successfully fetched submission snippet information',
+    status = HTTPStatusCode.OK,
+    data = _SubmissionSnippetGetData(
+      id = submissionSnippet.id,
+      student_id = submissionSnippet.student_id,
+      submission_id = submissionSnippet.submission_id,
+      uri = submissionSnippet.uri,
+      status = submissionSnippet.status,
+      created_at = submissionSnippet.created_at.timestamp()
+    )
+  ).to_dict(), HTTPStatusCode.OK
