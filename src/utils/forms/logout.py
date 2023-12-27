fa06de4ae36e523a90ab9fb346dd4850e22fcf01
AlexNg <taboez4life@gmail.com>
2023-12-27 16:56:11 +0800

    - Remove unneccesssary init

--- src/utils/forms/logout.py
+++ src/utils/forms/logout.py
@@ -5,9 +5,3 @@ class LogoutForm(FlaskForm):
   """Logout Form"""
 
   submit = SubmitField('Log Out')
-
-  def validate(self, *args, **kwargs) -> bool:
-    field_validation = super().validate(*args, **kwargs)
-    if not field_validation: return False
-    
-    return True
