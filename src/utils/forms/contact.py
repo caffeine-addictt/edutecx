4f75951cd9ef21197c2bc58c9296ccf35ba7f70c
AlexNg <taboez4life@gmail.com>
2023-12-28 14:46:17 +0800

    + Allow name to be accessed

--- src/utils/forms/contact.py
+++ src/utils/forms/contact.py
@@ -6,11 +6,11 @@ from wtforms.validators import DataRequired, Email, Length
 class ContactForm(FlaskForm):
   """Contact Us Form"""
 
-  form_type   = SelectField('Form Type', choices = [(1, 'Feedback'), (2, 'Bug Report'), (3, 'User Report')])
-  email       = StringField('Email', validators = [DataRequired(), Length(1, 64), Email('Invalid Email', check_deliverability = True)])
-  subject     = StringField('Title', validators = [DataRequired(), Length(1, 64)])
-  message     = StringField('Message', validators = [DataRequired(), Length(1, 300)])
-  submit      = SubmitField('Log In')
+  form_type   = SelectField(name = 'Form Type', choices = [(1, 'Feedback'), (2, 'Bug Report'), (3, 'User Report')])
+  email       = StringField(name = 'Email', validators = [DataRequired(), Length(1, 64), Email('Invalid Email', check_deliverability = True)])
+  subject     = StringField(name = 'Title', validators = [DataRequired(), Length(1, 64)])
+  message     = StringField(name = 'Message', validators = [DataRequired(), Length(1, 300)])
+  submit      = SubmitField(name = 'Log In')
 
   def validate(self, *args, **kwargs) -> bool:
     field_validation = super().validate(*args, **kwargs)
--- src/utils/forms/login.py
+++ src/utils/forms/login.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Email, Length
 class LoginForm(FlaskForm):
   """Login Form"""
 
-  email       = StringField('Email', validators = [DataRequired(), Length(1, 64), Email()])
-  password    = PasswordField('Password', validators = [DataRequired(), Length(1, 20)])
-  remember_me = BooleanField('Keep me logged in')
-  submit      = SubmitField('Log In')
+  email       = StringField(name = 'Email', validators = [DataRequired(), Length(1, 64), Email()])
+  password    = PasswordField(name = 'Password', validators = [DataRequired(), Length(1, 20)])
+  remember_me = BooleanField(name = 'Keep me logged in')
+  submit      = SubmitField(name = 'Log In')
