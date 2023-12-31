537c21fc5cb3882a39b3b1c0fad2cfd76b07dec0
AlexNg <taboez4life@gmail.com>
2023-12-31 15:06:21 +0800

    + Added multiplier and used_by

--- src/database/discount.py
+++ src/database/discount.py
@@ -6,7 +6,7 @@ from src import db
 
 import uuid
 from datetime import datetime
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import (
@@ -20,6 +20,7 @@ from sqlalchemy import (
 
 # Import TokenModel at runtime to prevent circular imports
 if TYPE_CHECKING:
+  from .sale import SaleModel
   from .textbook import TextbookModel
 
 
@@ -33,17 +34,20 @@ class DiscountModel(db.Model):
   id         : Mapped[str] = mapped_column(String, primary_key = True, unique = True, nullable = False, default = lambda: uuid.uuid4().hex)
   textbook_id: Mapped[Optional[str]] = mapped_column(ForeignKey('textbook_table.id'), nullable = False)
 
-  code    : Mapped[str]                       = mapped_column(String, nullable = False)
-  used    : Mapped[float]                     = mapped_column(Float, nullable = False, default = 0.0)
-  limit   : Mapped[Optional[int]]             = mapped_column(Integer, nullable = True)
-  textbook: Mapped[Optional['TextbookModel']] = relationship('TextbookModel', back_populates = 'discounts')
+  code      : Mapped[str]                       = mapped_column(String, nullable = False)
+  used      : Mapped[float]                     = mapped_column(Float, nullable = False, default = 0.0)
+  used_by   : Mapped[List['SaleModel']]         = relationship('SaleModel', back_populates = 'used_discount')
+  limit     : Mapped[Optional[int]]             = mapped_column(Integer, nullable = True)
+  multiplier: Mapped[float]                     = mapped_column(Float, nullable = False, default = 1.0)
+  textbook  : Mapped[Optional['TextbookModel']] = relationship('TextbookModel', back_populates = 'discounts')
 
-  expires_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nulalble = True)
+  expires_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable = True)
   created_at: Mapped[datetime] = mapped_column(DateTime, nullable = False, default = datetime.utcnow)
 
   def __init__(
     self,
     code: str,
+    multiplier: float,
     textbook: Optional['TextbookModel'] = None,
     *,
     expires_at: Optional[datetime] = None,
@@ -56,13 +60,23 @@ class DiscountModel(db.Model):
     ----------
     `code: str`, required
 
+    `multiplier: float`, required
+      The discount multiplier from x0 to x3
+
     `textbook: TextbookModel`, optional (defaults to None)
 
     `expires_at: datetime`, optional (defaults to None)
 
     `limit: int`, optional (defaults to None)
+
+    Raises
+    ------
+    AssertionError : If `multiplier` is not between 0 and 3
     """
+    assert 0 <= multiplier <= 3, 'Multiplier must be between 0 and 3'
+
     self.code = code
+    self.multiplier = multiplier
     self.textbook = textbook
     self.expires_at = expires_at
     self.limit = limit
