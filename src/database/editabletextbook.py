7cd13d5dc100de1cd5604b327d5b617ad4fa7b68
AlexNg <taboez4life@gmail.com>
2024-01-03 19:42:42 +0800

    + Added editabletxtbook status and enums

--- src/database/editabletextbook.py
+++ src/database/editabletextbook.py
@@ -8,11 +8,12 @@ from src.service.cdn_provider import cloneTextbook, updateEditableTextbook, dele
 import uuid
 from thread import Thread
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Literal
 from werkzeug.datastructures import FileStorage
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import (
+  Enum,
   String,
   DateTime,
   ForeignKey
@@ -22,20 +23,25 @@ if TYPE_CHECKING:
   from .user import UserModel
   from .textbook import TextbookModel
 
+
+EditableTextbookUploadStatus = Literal['Uploading', 'Uploaded']
+EnumEditableTextbookUploadStatus = Enum('Uploading', 'Uploaded', name = 'EditableTextbookUploadStatus')
+
+
 class EditableTextbookModel(db.Model):
   """Editable Textbook Model"""
 
   __tablename__ = 'editable_textbook_table'
 
-  id: Mapped[str] = mapped_column(String, primary_key = True, unique = True, default = lambda: uuid.uuid4().hex)
-  user_id: Mapped[str] = mapped_column(ForeignKey('user_table.id'))
+  id         : Mapped[str] = mapped_column(String, primary_key = True, unique = True, default = lambda: uuid.uuid4().hex)
+  user_id    : Mapped[str] = mapped_column(ForeignKey('user_table.id'))
   textbook_id: Mapped[str] = mapped_column(ForeignKey('textbook_table.id'))
 
-  uri      : Mapped[str] = mapped_column(String, nullable = True)
-  iuri     : Mapped[str] = mapped_column(String, nullable = True)
-  status   : Mapped[str] = mapped_column(String, nullable = False, default = 'Uploading')
-  user: Mapped['UserModel'] =  relationship('UserModel', back_populates = 'textbooks')
-  origin: Mapped['TextbookModel'] = relationship('TextbookModel', back_populates = 'derrived')
+  uri      : Mapped[str]                          = mapped_column(String, nullable = True)
+  iuri     : Mapped[str]                          = mapped_column(String, nullable = True)
+  status   : Mapped[EditableTextbookUploadStatus] = mapped_column(EnumEditableTextbookUploadStatus, nullable = False, default = 'Uploading')
+  user     : Mapped['UserModel']                  = relationship('UserModel', back_populates = 'textbooks')
+  origin   : Mapped['TextbookModel']              = relationship('TextbookModel', back_populates = 'derrived')
 
   created_at: Mapped[datetime] = mapped_column(DateTime, nullable = False, default = datetime.utcnow)
 
@@ -73,7 +79,7 @@ class EditableTextbookModel(db.Model):
 
   def update(self, file: FileStorage) -> None:
     """Update the file content"""
-    self.status = 'Updating'
+    self.status = 'Uploading'
 
     job = Thread(updateEditableTextbook, kwargs = {
       'fileLocation': self.iuri,
