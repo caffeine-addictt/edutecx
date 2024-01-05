ea5209d1b2808c49b1752201aaeb04dbcfe00a68
AlexNg <taboez4life@gmail.com>
2024-01-05 15:55:05 +0800

    + Added buttons to rehydrate and stream svg string

--- src/static/(admin)/textbook.js
+++ src/static/(admin)/textbook.js
@@ -1,12 +1,29 @@
 
 /**
- * Fetch and set graph URI
+ * Track how many times graph is drawn to apply weighted cooldown
+ * @type {number}
+ */
+let times = 0;
+
+
+
+
+/**
+ * Fetch and draw graph
  * @returns {Promise<void>}
  */
 const fetchGraphURI = async () => {
-  response = await fetch('/dashboard/graph', {
+  $('#graph__button').attr('disabled', true);
+  $('#graph__button').text('Drawing...');
+  renderToast('Drawing Graph...', 'info');
+
+  $('#svg-render').empty();
+
+  /**
+   * @type {Response}
+   */
+  const response = await fetch('/dashboard/graph', {
     method: 'POST',
-    credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
       'X-CSRF-TOKEN': getAccessToken()
@@ -14,12 +31,36 @@ const fetchGraphURI = async () => {
     body: JSON.stringify({
       "graphFor": "Textbook"
     })
-  }).then(async res => await res.json())
+  })
 
-  if (response.status !== 200) throw new Error(response.message)
+  if (!response.ok) {
+    renderToast('Failed to fetch Graph', 'danger');
+  }
+  else {
+    const svg = await response.text();
+  
+    $('#svg-render').empty();
+    $('#svg-render').append(htmlToElement(svg));
+    console.log('Fetched SVG')
+  };
 
-  $('#graph').attr('src', response.data.uri)
-  console.log('Fetched URI: ' + response.data.uri)
+  // Countdown
+  let countdown;
+  let iterations = 0;
+  const iterationsRequired = ((2 ** times) + 30);
+  countdown = setInterval(() => {
+    if (iterations === iterationsRequired) {
+      clearInterval(countdown);
+      $('#graph__button').text('Redraw Graph');
+      $('#graph__button').attr('disabled', false);
+    }
+    else {
+      $('#graph__button').text(`Redraw Graph (${iterationsRequired - iterations}s)`);
+      iterations++;
+    };
+  }, 1000);
+
+  times++;
 }
 
 
@@ -29,7 +70,13 @@ const fetchGraphURI = async () => {
  * Stream filtered query
  * @returns {Promise<void>}
  */
-const fetchUserData = async () => {
+const fetchTextbookData = async () => {
+  $('#textbook__button').attr('disabled', true);
+  $('#textbook__button').text('Fetching...');
+  renderToast('Fetching textbooks...', 'info');
+
+  $('#textbook__container').empty();
+
   let searchParams = ((new URL(location.href)).searchParams)
   let criteria = searchParams.get('criteria')
   criteria = ['or', 'and'].includes(criteria) ? criteria : 'or'
@@ -37,23 +84,78 @@ const fetchUserData = async () => {
   searchParams.set('requestFor', 'Textbook')
   searchParams.set('criteria', criteria)
 
-  response = await fetch(`/dashboard/get?${searchParams.toString()}`, {
+  /**
+   * @type {{
+   *   status : 200;
+   *   message: string;
+   *   data: Array.<{
+   *     id: string;
+   *     author_id: string;
+   *     title: string;
+   *     description: string;
+   *     categories: string[];
+   *     price: number;
+   *     discount: number;
+   *     uri: string;
+   *     status: 'Uploading' | 'Uploaded';
+   *     cover_image: string | null;
+   *     created_at: number;
+   *     updated_at: number;
+   *   }>;
+   * } | {
+   *   status: 500;
+   *   message: string;
+   * }}
+   */
+  const response = await fetch(`/dashboard/get?${searchParams.toString()}`, {
     method: 'GET',
     headers: {
       'X-CSRF-TOKEN': getAccessToken()
     }
   }).then(async res => await res.json())
 
-  if (response.status !== 200) throw new Error(response.message)
-
-  /* TODO: Inject data at runtime */
-  console.log('Fetched Data')
+  if (response.status !== 200) {
+    console.log(response.message);
+    renderToast('Failed to fetch textbooks!', 'danger');
+  }
+  else if (!response.data || response.data.length === 0) {
+    // Render no textbooks
+  }
+  else {
+    $('#textbook__container').empty();
+    response.data.forEach(textbook => {
+      // Render macros for textbooks
+      $('#textbook__container').append(`${textbook.title}`);
+    });
+  };
+  
+  // Countdown
+  let countdown;
+  let iterations = 0;
+  countdown = setInterval(() => {
+    if (iterations === 30) {
+      clearInterval(countdown);
+      $('#textbook__button').text('Reload Textbooks');
+      $('#textbook__button').attr('disabled', false);
+    }
+    else {
+      $('#textbook__button').text(`Reload Textbooks (${30 - iterations}s)`);
+      iterations++;
+    };
+  }, 1000);
 }
 
 
 
 
 // Run in parallel
-$(() => Promise.all([ fetchGraphURI(), fetchUserData() ])
+$(() => Promise.all([ fetchGraphURI(), fetchTextbookData() ])
   .then(values => console.log(values))
-  .catch(err => console.log(err)))
+  .catch(err => console.log(err)));
+
+
+$(() => {
+  // Hooks
+  $('#graph__button').on('click', async e => await fetchGraphURI());
+  $('#textbook__button').on('click',  async e => await fetchTextbookData());
+});
