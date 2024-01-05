19505a99f3322fe39403ebbf8916f2340c511241
AlexNg <taboez4life@gmail.com>
2024-01-05 23:07:10 +0800

    + Redirect to pricing page

--- src/routes/classroom.py
+++ src/routes/classroom.py
@@ -39,7 +39,7 @@ def classroom(user: UserModel, id: str):
 
 
 @app.route('/classrooms/new', methods = ['GET', 'POST'])
-@auth_provider.require_educator
+@auth_provider.require_educator(unauthorized_redirect = '/pricing')
 def classroom_new(user: UserModel):
   form = ClassroomCreateForm(request.form)
   return render_template('(classroom)/classroom_new.html', form = form)
