d7f6c758df84670482c331e3c6c84dde83bf0e8f
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-01-09 15:50:14 +0800

    created classroomEditForm, removed validation

--- src/utils/forms/classroom.py
+++ src/utils/forms/classroom.py
@@ -10,13 +10,11 @@ class ClassroomCreateForm(FlaskForm):
   inviteEnabled = BooleanField(name = 'Invite Enabled', default = True)
   agreeToTOS    = BooleanField(name = 'I agree to the <a href="/">terms of service</a>', validators = [DataRequired()])
   submit        = SubmitField(name = 'Create')
+
+
+class ClassroomEditForm(FlaskForm):
+  """Create classroom"""
+  title         = StringField(name = 'Title', validators = [DataRequired(), Length(1, 64)])
+  description   = StringField(name = 'Description', validators = [DataRequired(), Length(1, 300)])
+  inviteEnabled = BooleanField(name = 'Invite Enabled', default = True)
   confirmchange = SubmitField(name = 'Confirm')
-
-  def validate(self, *args, **kwargs) -> bool:
-    field_validation = super().validate(*args, **kwargs)
-    if not field_validation: return False
-
-    # TODO: Validation
-    return True
-
-
