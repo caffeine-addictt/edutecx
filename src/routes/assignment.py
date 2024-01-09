ff40aa8fc5ff788b259f54cc292d5e0fb9dc4403
Mani <maniman503@gmail.com>
2024-01-09 19:55:32 +0800

    Assingment create,edit and delete

--- src/routes/assignment.py
+++ src/routes/assignment.py
@@ -24,3 +24,24 @@ def assignments(user: UserModel):
 def assignment(user: UserModel, id: str):
   id = escape_id(id)
   return render_template('(assignment)/assignment.html')
+
+
+@app.route('/assignments/new', methods = ['GET'])
+@auth_provider.require_educator(unauthorized_redirect = '/pricing')
+def assignment_new(user: UserModel):
+  return render_template('(assignment)/assignment_new.html')
+
+
+@app.route('/assignments/edit/<string:id>', methods=['GET'])
+@auth_provider.require_educator(unauthorized_redirect='/pricing')
+def assignment_edit(user: UserModel, id: str):
+    id = escape_id(id)
+    assignment = AssignmentModel.query.filter(AssignmentModel.id == id).first()
+
+    if not isinstance(assignment, AssignmentModel):
+        return render_template('(assignment)/assignment_error.html')
+
+    if (not assignment.classroom.is_privileged(user)) and (user.privilege != 'Admin'):
+        return render_template('(assignment)/assignment_error.html', message='You are not authorized to edit this assignment')
+
+    return render_template('(assignment)/assignment_edit.html')
