79408494bbcdd92ab35ec446e24db2c39cbe019b
AlexNg <taboez4life@gmail.com>
2024-01-09 12:12:18 +0800

    Bugfix wrong order

--- src/routes/store.py
+++ src/routes/store.py
@@ -43,7 +43,7 @@ def store(user: UserModel | None):
 
 
 # Store focus view
-@app.route('/store/<id:string>')
+@app.route('/store/<string:id>')
 @auth_provider.optional_login
 def store_focused(user: UserModel | None, textbook_id: str):
   textbook_id = escape_id(textbook_id)
