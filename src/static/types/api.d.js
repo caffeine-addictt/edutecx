bf476e8e32b54bdd0b3ce5f83e04e9dcd3618d56
AlexNg <taboez4life@gmail.com>
2024-01-29 21:32:47 +0800

    Move typing definition

--- src/static/types/api.d.js
+++ src/static/types/api.d.js
@@ -4,4 +4,17 @@
  */
 
 
-
+/**
+ * @typedef {Object} TextbookGetData
+ * @property {string} id
+ * @property {string} author_id
+ * @property {string} title
+ * @property {string} description
+ * @property {string[]} categories
+ * @property {number} price
+ * @property {string} uri
+ * @property {'Available' | 'Unavailable' | 'DMCA'} status
+ * @property {string | null} cover_image
+ * @property {number} created_at
+ * @property {number} updated_at
+ */
