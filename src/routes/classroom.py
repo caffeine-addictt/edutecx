166e7f9c9b95e6ba471c0d9fa273d4bd408b46e4
chloelkl <99401053+chloelkl@users.noreply.github.com>
2024-01-30 17:27:20 +0800

    default form values for edit

--- src/routes/classroom.py
+++ src/routes/classroom.py
@@ -60,6 +60,9 @@ def classroom_edit(user: UserModel, id: str):
     return render_template('(classroom)/classroom_error.html', message = 'You are not authorized to edit this classroom')
 
   form = ClassroomEditForm(request.form)
+  form.title.data = classroom.title
+  form.description.data = classroom.description
+  form.inviteEnabled.data = classroom.invite_enabled
   classroom = ClassroomModel.query.filter(ClassroomModel.id == id).first()
   return render_template('(classroom)/classroom_edit.html', form = form, classroom = classroom)
 
