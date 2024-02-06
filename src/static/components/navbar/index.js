50a7f947d4272e11e66e2bd62e264599821b50de
AlexNg <taboez4life@gmail.com>
2024-02-06 11:32:49 +0800

    feat: Made navbar responsive to login and user status

--- src/static/components/navbar/index.js
+++
@@ -1,8 +0,0 @@
-
-
-// On Render
-(() => {
-  $("#login-navlink").attr('href', window.location.pathname === '/' ? '/login' : `/login?callbackURI=${encodeURI(window.location.pathname)}`);
-  $("#register-navlink").attr('href', window.location.pathname === '/' ? '/register' : `/register?callbackURI=${encodeURI(window.location.pathname)}`);
-});
-
