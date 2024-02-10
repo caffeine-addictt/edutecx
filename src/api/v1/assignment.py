7aaedeac09f2482e66e2a9aa62ccdac82b71e36a
AlexNg <taboez4life@gmail.com>
2024-02-10 19:53:07 +0800

    feat: v1/list

--- src/api/v1/assignment.py
+++ src/api/v1/assignment.py
@@ -8,6 +8,8 @@ from src.service.auth_provider import require_login
 from src.utils.http import HTTPStatusCode
 from src.utils.ext import utc_time
 from src.utils.api import (
+  AssignmentListRequest,
+  AssignmentListReply,
   AssignmentGetRequest,
   AssignmentGetReply,
   _AssignmentGetData,
@@ -33,6 +35,71 @@ basePath: str = '/api/v1/assignment'
 auth_limit = limiter.shared_limit(
   '100 per hour', scope=lambda _: request.host, key_func=util.get_remote_address
 )
+
+
+@app.route(f'{basePath}/list', methods=['GET'])
+@auth_limit
+@require_login
+def assignment_list_api(user: UserModel):
+  req = AssignmentListRequest(request)
+
+  dateRange: tuple[datetime, datetime] = (
+    datetime.fromtimestamp(req.createdLower)
+    if float('inf') != req.createdLower
+    else utc_time.skip('1day'),
+    datetime.fromtimestamp(req.createdUpper)
+    if float('inf') != req.createdUpper
+    else utc_time.skip('1day'),
+  )
+
+  if dateRange[0] > dateRange[1]:
+    return GenericReply(
+      message='createdLower is larger than createdUpper',
+      status=HTTPStatusCode.BAD_REQUEST,
+    ).to_dict(), HTTPStatusCode.BAD_REQUEST
+
+  if req.query == 'None':
+    req.query = ''
+
+  # Build query
+  query = [
+    and_(
+      dateRange[0] <= AssignmentModel.created_at,
+      AssignmentModel.created_at <= dateRange[1],
+    ),
+    or_(
+      AssignmentModel.id.contains(req.query),
+      AssignmentModel.title.contains(req.query),
+      AssignmentModel.requirement.contains(req.query),
+    ),
+  ]
+
+  filtered = AssignmentModel.query.filter(
+    and_(
+      (AssignmentModel.classroom.members.contains(user)),
+      and_(*query) if req.criteria == 'and' else or_(*query),
+    )
+  ).paginate(page=req.page, error_out=False)
+
+  return AssignmentListReply(
+    message='Successfully fetched assignment list',
+    status=HTTPStatusCode.OK,
+    data=[
+      _AssignmentGetData(
+        id=assignment.id,
+        classroom_id=assignment.classroom.id,
+        title=assignment.title,
+        description=assignment.description,
+        due_date=assignment.due_date.timestamp(),
+        textbooks=[i.id for i in assignment.textbooks],
+        requirement=assignment.requirement,
+        submissions=[i.id for i in assignment.submissions],
+        created_at=assignment.created_at.timestamp(),
+        updated_at=assignment.updated_at.timestamp(),
+      )
+      for assignment in filtered
+    ],
+  ).to_dict(), HTTPStatusCode.OK
 
 
 @app.route(f'{basePath}/get', methods=['GET'])
@@ -79,7 +146,7 @@ def assignment_create_api(user: UserModel):
   req = AssignmentCreateRequest(request)
 
   # Validate due_date
-  if (req.due_date != 'infinity' and not isinstance(req.due_date, int)) or (
+  if (req.due_date != 'infinity' and not isinstance(req.due_date, (int, float))) or (
     utc_time.get().timestamp() >= int(req.due_date)
   ):
     return GenericReply(
@@ -87,7 +154,9 @@ def assignment_create_api(user: UserModel):
     ).to_dict(), HTTPStatusCode.BAD_REQUEST
 
   due_date = (
-    datetime.fromtimestamp(req.due_date) if isinstance(req.due_date, int) else None
+    datetime.fromtimestamp(req.due_date)
+    if isinstance(req.due_date, (int, float))
+    else None
   )
 
   # Validate classroom
